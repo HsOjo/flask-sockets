16b56e23255f409024bd8bbd934dc8c8a98f0fab
HsOjo <1134031392@qq.com>
2023-08-10 03:22:21 +0800

    [fix] TypeError: a bytes-like object is required, not 'str'

--- flask_sockets.py
+++ flask_sockets.py
@@ -119,7 +119,7 @@ if ('Worker' in locals() and 'PyWSGIHandler' in locals() and
 
     class GunicornWebSocketHandler(PyWSGIHandler, WebSocketHandler):
         def log_request(self):
-            if '101' not in self.status:
+            if b'101' not in self.status:
                 super(GunicornWebSocketHandler, self).log_request()
 
     Worker.wsgi_handler = GunicornWebSocketHandler
